5fd681b001e13d239026320288cb578918e54220
jyothsna9797 <pamarthi.jyothsna97@gmail.com>
2022-11-29 20:21:50 -0600

    updated authentication for login page

--- routes/pastry.js
+++ routes/pastry.js
@@ -13,6 +13,20 @@ var express = require('express');
 const pastry_controlers= require('../controllers/pastry'); 
 var router = express.Router(); 
  
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET Pastry */ 
 router.get('/', pastry_controlers.pastry_view_all_Page ); 
+
+router.get('/update', secured, pastry_controlers.pastry_update_Page); 
+
 module.exports = router; 
