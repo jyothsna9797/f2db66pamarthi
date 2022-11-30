19bb02052e043ca544e633242af4a05ca6808b6b
jyothsna9797 <pamarthi.jyothsna97@gmail.com>
2022-11-30 10:59:23 -0600

    commiting the updated code for login page

--- routes/index.js
+++ routes/index.js
@@ -42,11 +42,15 @@ router.get('/login', function(req, res) {
     res.render('login', { title: 'Pastry App Login', user : req.user }); 
 }); 
  
-router.post('/login', passport.authenticate('local'), function(req, res) { 
-    if(req.session.returnTo) 
-      res.redirect(req.session.returnTo); 
-    res.redirect('/'); 
-}); 
+router.post('/login', passport.authenticate('local',{keepSessionInfo: true}), function(req, res) {
+
+  if(req.session.returnTo){
+
+    res.redirect(req.session.returnTo);}
+
+  res.redirect('/');
+
+});
  
 router.get('/logout', function(req, res, next) {          // use post or delete for better safety
   req.logout( function(err) {
--- routes/pastry.js
+++ routes/pastry.js
@@ -7,26 +7,38 @@
 // });
 
 
-// module.exports = router;
 
-var express = require('express'); 
-const pastry_controlers= require('../controllers/pastry'); 
-var router = express.Router(); 
- 
-// A little function to check if we have an authorized user and continue on 
-//or 
-// redirect to login. 
-const secured = (req, res, next) => { 
-    if (req.user){ 
-      return next(); 
-    } 
-    req.session.returnTo = req.originalUrl; 
-    res.redirect("/login"); 
-  } 
 
-/* GET Pastry */ 
-router.get('/', pastry_controlers.pastry_view_all_Page ); 
+var express = require('express');
+const pastry_controlers= require('../controllers/pastry');
+var router = express.Router();
 
-router.get('/update', secured, pastry_controlers.pastry_update_Page);
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
+/* GET costumes */
+router.get('/', pastry_controlers.pastry_view_all_Page);
+
+/* GET detail juice page */
+router.get('/detail', pastry_controlers.pastry_view_one_Page);
+
+
+/* GET create juice page */
+router.get('/create', pastry_controlers.pastry_create_Page);
+
+
+/* GET create update page */
+router.get('/update',secured, pastry_controlers.pastry_update_Page);
+
+/* GET delete juice page */
+router.get('/delete',pastry_controlers.pastry_delete_Page);
 
 module.exports = router;
